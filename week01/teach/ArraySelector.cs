99c460db12cc5055c9dd45bedb9fe09ef35f0b0c
cesarv <cvelascodo@gmail.com>
2026-01-07 00:09:33 -0600

    add taxt

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,23 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
+        List<int> result = new();
+        int index1 = 0;
+        int index2 = 0;
+
+        foreach (int s in select)
+        {
+            if (s == 1)
+            {
+                result.Add(list1[index1]);
+                index1++;
+            }
+            else if (s == 2)
+            {
+                result.Add(list2[index2]);
+                index2++;
+            }
+        }
         return [];
     }
 }
--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -19,6 +19,18 @@ public static class Divisors {
     private static List<int> FindDivisors(int number) {
         List<int> results = new();
         // TODO problem 1
+        
+        results.Add(1);
+
+        // Loop from 2 up to number-1
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                results.Add(i);
+            }
+        }
+
         return results;
     }
 }
